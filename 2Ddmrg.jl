98a09c3c8928981c53a611f5a9429d8a53fbfe07
sandyirani <31449386+sandyirani@users.noreply.github.com>
2018-02-05 10:23:48 -0800

    new files

---
+++ 2Ddmrg.jl
@@ -0,0 +1,1 @@
+include("gridUtilities.jl")
---
+++ gridUtilities.jl
@@ -0,0 +1,11 @@
+function getCoords(j)
+  if (j < 1 || j > N)
+    return(0,0)
+  end
+end
+
+function getCoords2(j)
+  if (j < 1 || j > N)
+    return(0,0)
+  end
+end
